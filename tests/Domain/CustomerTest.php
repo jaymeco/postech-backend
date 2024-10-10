00da649e02e29674f2b51697eab00d7905a3c2a4
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-10 20:35:30 -0300

    test(CustomerTest): Ajusta teste para a nova construcao da entidade de cliente.

--- tests/Domain/CustomerTest.php
+++ tests/Domain/CustomerTest.php
@@ -3,23 +3,22 @@
 namespace Tests\Domain;
 
 use Core\Domain\Entities\Customer;
-use Core\Domain\Entities\User;
-use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
-use Ramsey\Uuid\Uuid as UuidFaker;
 
 class CustomerTest extends TestCase
 {
     public function test_should_create_customer()
     {
-        $userUuid = UuidFaker::uuid4();
+        $email = 'simple@email.com';
+        $name = 'Simple Name';
         $cpf = "00000000000";
 
-        $customer = Customer::create($cpf, $userUuid);
+        $customer = Customer::create($cpf, $name, $email);
 
         $this->assertEquals($cpf, $customer->getCpf()->getValue());
-        $this->assertEquals($userUuid, $customer->getUserUuid()->getValue());
         $this->assertNotNull($customer->getUuid());
         $this->assertEquals('REGISTERED', $customer->getType()->getValue());
+        $this->assertEquals($name, $customer->getName()->getValue());
+        $this->assertEquals($email, $customer->getEmail()->getValue());
     }
 }
