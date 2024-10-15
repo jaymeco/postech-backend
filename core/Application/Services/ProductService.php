d4f93319414d6972a8ad82345ad523002d5d6158
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-14 21:36:10 -0300

    feature(core/Application/Services/ProductService): Implementa metodo update.

--- core/Application/Services/ProductService.php
+++ core/Application/Services/ProductService.php
@@ -4,6 +4,7 @@ namespace Core\Application\Services;
 
 use Core\Application\Contracts\Repositories\CategoryRepository;
 use Core\Application\Contracts\Repositories\ProductRepository;
+use Core\Domain\Base\Helpers\DirtyValuesHelper;
 use Core\Domain\Entities\Product;
 
 class ProductService
@@ -22,5 +23,31 @@ class ProductService
         $this->productRepository->save($product);
 
         return $product;
+    }
+
+    public function update(
+        string $productUuid,
+        ?string $name = null,
+        ?string $description = null,
+        ?string $categoryUuid = null,
+        ?string $imageUri = null,
+        ?float $price = null
+    ) {
+        $product = $this->productRepository->getByUuid($productUuid);
+        $category = $product->getCategory();
+        if (!is_null($categoryUuid)) {
+            $category = $this->categoryRepository->getByUuid($categoryUuid);
+        }
+
+        $updatedProduct = Product::restore(
+            $product->getUuid()->getValue(),
+            DirtyValuesHelper::resolve($product->getName()->getValue(), $name),
+            DirtyValuesHelper::resolve($product->getDescription(), $description),
+            $category,
+            DirtyValuesHelper::resolve($product->getImageUri(), $imageUri),
+            DirtyValuesHelper::resolve($product->getPrice()->getValue(), $price),
+        );
+
+        $this->productRepository->update($updatedProduct);
     }
 }
