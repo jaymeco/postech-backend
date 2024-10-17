c33100b6f2d80648d7096a4ab38855125fa83bbb
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-17 20:51:29 -0300

    feature(core/Application/Contracts/Repositories/OrderRepository): Adiciona assinatura para o metodo de consulta por uuid.
    - Adiciona assinatura para o metodo de atualizacao.

--- core/Application/Contracts/Repositories/OrderRepository.php
+++ core/Application/Contracts/Repositories/OrderRepository.php
@@ -7,4 +7,8 @@ use Core\Domain\Entities\Order;
 interface OrderRepository
 {
     public function save(Order $order): void;
+
+    public function getByUuid(string $uuid): Order;
+
+    public function update(Order $order): void;
 }
