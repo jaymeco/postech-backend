ac8a44bd4f1964504de2127b9c482719c26c601d
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-20 15:40:05 -0300

    fix(core/Domain/Base/Helpers/DirtyValuesHelper): Corrige condicional para a validacao de dados alterados.

--- core/Domain/Base/Helpers/DirtyValuesHelper.php
+++ core/Domain/Base/Helpers/DirtyValuesHelper.php
@@ -6,7 +6,7 @@ abstract class DirtyValuesHelper
 {
     public static function resolve($original, $new = null)
     {
-        if (!is_null($new) && $new == $original) {
+        if (!is_null($new) && $new != $original) {
             return $new;
         }
 
