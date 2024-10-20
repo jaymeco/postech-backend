3794d042453b3a0a48a22494ff81cde614f0c7e8
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-20 15:40:47 -0300

    test(tests/Application/ProductServiceTest): Cria teste para a atualizacao de produto.

--- tests/Application/ProductServiceTest.php
+++ tests/Application/ProductServiceTest.php
@@ -6,8 +6,6 @@ use App\Infra\Repositories\Memory\CategoryMemoryRepository;
 use App\Infra\Repositories\Memory\ProductMemoryRepository;
 use Core\Application\Services\ProductService;
 use Core\Domain\Base\Enums\CategoryEnum;
-use Core\Domain\Entities\Category;
-use Core\Domain\Entities\Product;
 use PHPUnit\Framework\TestCase;
 use Tests\Fakes\Domain\ProductFaker;
 
@@ -33,6 +31,31 @@ class ProductServiceTest extends TestCase
         $this->assertEquals(CategoryEnum::SNACK->key(), $product->getCategory()->getUuid()->getValue());
         $this->assertEquals($fake->imageUri, $product->getImageUri());
         $this->assertEquals($fake->price, $product->getPrice()->getValue());
+    }
 
+    public function test_should_update_product()
+    {
+        $service = new ProductService(new ProductMemoryRepository(), new CategoryMemoryRepository());
+        $fake = new ProductFaker();
+        $name = 'Suco de goiaba';
+        $category = CategoryEnum::DRINK->key();
+        $price = 2.00;
+        $description = 'Um suco';
+
+        $created = $service->create(
+            $fake->name,
+            $fake->description,
+            CategoryEnum::SNACK->key(),
+            $fake->imageUri,
+            $fake->price
+        );
+        $service->update($created->getUuid()->getValue(), $name, $description, $category, null, $price);
+        $product = $service->getByUuid($created->getUuid()->getValue());
+
+        $this->assertEquals($name, $product->getName()->getValue());
+        $this->assertEquals($description, $product->getDescription());
+        $this->assertEquals($category, $product->getCategory()->getUuid()->getValue());
+        $this->assertEquals($fake->imageUri, $product->getImageUri());
+        $this->assertEquals($price, $product->getPrice()->getValue());
     }
 }
