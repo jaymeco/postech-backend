043f2c89b07987156073d577ac8ac1c75ab020fb
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-20 18:19:38 -0300

    feature(app/Infra/Repositories/Memory/CustomerMemoryRepository): Implementa metodo de consulta pelo uuid.

--- app/Infra/Repositories/Memory/CustomerMemoryRepository.php
+++ app/Infra/Repositories/Memory/CustomerMemoryRepository.php
@@ -4,10 +4,11 @@ namespace App\Infra\Repositories\Memory;
 
 use Core\Application\Contracts\Repositories\CustomerRepository;
 use Core\Domain\Entities\Customer;
+use Error;
 
 class CustomerMemoryRepository implements CustomerRepository
 {
-    /***@var Custormer[] */
+    /***@var Customer[] */
     private array $database;
 
     public function __construct()
@@ -18,5 +19,19 @@ class CustomerMemoryRepository implements CustomerRepository
     public function save(Customer $customer): void
     {
         $this->database[] = $customer;
+    }
+
+    public function getByUuid(string $uuid): Customer
+    {
+        $founds = array_filter($this->database, function ($customer) use ($uuid) {
+            return $customer->getUuid()->equals($uuid);
+        });
+
+        if (count($founds) <= 0 || is_null($founds)) {
+            // TODO Criar exceptions da aplicacao
+            throw new Error('Not found');
+        }
+
+        return $founds[array_key_first($founds)];
     }
 }
