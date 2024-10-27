49693ab6633d5890277d264bf05146bc5fb8dba9
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-27 14:18:19 -0300

    feature(core/Domain/ValueObjects/CustomerType): Implementa metodo restore.

--- core/Domain/ValueObjects/CustomerType.php
+++ core/Domain/ValueObjects/CustomerType.php
@@ -4,6 +4,11 @@ namespace Core\Domain\ValueObjects;
 
 class CustomerType extends ValueObject
 {
+    public static function restore(string $type)
+    {
+        return new static($type);
+    }
+
     public static function guest()
     {
         return new static("GUEST");
