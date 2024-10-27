22a9dc300cf1ce0e49a42e4c2931199577171364
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-27 14:35:44 -0300

    feature(app/Providers/AppServiceProvider): Registra singleton para o customer repository.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace App\Providers;
 
+use App\Infra\Repositories\Eloquent\CustomerEloquentRepository;
+use App\Infra\Repositories\Memory\CustomerMemoryRepository;
+use Core\Application\Contracts\Repositories as RepositoriesContracts;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public array $singletons = [
+        RepositoriesContracts\CustomerRepository::class => CustomerEloquentRepository::class,
+    ];
     /**
      * Register any application services.
      */
