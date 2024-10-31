10503a03989c7c8f828c0b08c212819e4655b195
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-31 20:57:45 -0300

    feature(app/Http/Customer/Controllers/OrderController): Implementa action para a rota de checkout do pedido.

--- app/Http/Customer/Controllers/OrderController.php
+++ app/Http/Customer/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Customer\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Customer\Requests\CreateOrderRequest;
 use Core\Application\UseCases\CreateOrderUseCase;
+use Core\Application\UseCases\MakeCheckoutUseCase;
 
 class OrderController extends Controller
 {
@@ -17,5 +18,14 @@ class OrderController extends Controller
         $order = $useCase->execute($request->getProducts(), $request->getCustomerCpf());
 
         return response()->json($order, 201);
+    }
+
+    public function checkout(string $orderUuid)
+    {
+        $useCase = app(MakeCheckoutUseCase::class);
+
+        $useCase->execute($orderUuid);
+
+        return response()->json([], 204);
     }
 }
