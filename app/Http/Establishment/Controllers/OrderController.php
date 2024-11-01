f9afee2cf1835c0d1b308e0c371c405315c32485
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-31 21:31:07 -0300

    feature(app/Http/Establishment/Controllers/OrderController): Implementa action para a rota de listagem de pedidos.

--- app/Http/Establishment/Controllers/OrderController.php
+++ app/Http/Establishment/Controllers/OrderController.php
@@ -3,10 +3,36 @@
 namespace App\Http\Establishment\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Establishment\Requests\CreateOrderRequest;
-use Core\Application\UseCases\CreateOrderUseCase;
+use Core\Application\Contracts\Services\OrderService;
 
 class OrderController extends Controller
 {
-    public function __construct() {}
+    public function __construct(
+        private readonly OrderService $service,
+    ) {}
+
+    public function list()
+    {
+        $orders = $this->service->getAll();
+
+        return response()
+            ->json(
+                array_map(fn($order) => [
+                    'uuid' => $order->getUuid()->getValue(),
+                    'code' => $order->getCode()->getValue(),
+                    'status' => ['uuid' => $order->getStatus()->getUuid()->getValue(), 'name' => $order->getStatus()->getName()->getValue()],
+                    'ordered_at' => $order->getOrderedAt(),
+                    'price' => $order->getPrice()->getValue(),
+                    'products' => array_map(fn ($product) => [
+                        'uuid' => $product->getUuid()->getValue(),
+                        'name' => $product->getName()->getValue(),
+                        'descritption' => $product->getDescription(),
+                        'image_uri' => $product->getImageUri(),
+                        'price' => $product->getPrice()->getValue(),
+                        'category' => ['uuid' => $product->getCategory()->getUuid()->getValue(), 'name' => $product->getCategory()->getName()->getValue()],
+                    ], $order->getProducts())
+                ], $orders),
+                200,
+            );
+    }
 }
