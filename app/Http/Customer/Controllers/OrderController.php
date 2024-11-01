c3d0012314516379e0277ab03beb020fb9548f8e
Jaymeco <jayme.anunciacao@gmail.com>
2024-10-31 21:54:22 -0300

    feature(app/Http/Customer/Controllers/OrderController): Implementa action para a rota de consulta de um pedido.

--- app/Http/Customer/Controllers/OrderController.php
+++ app/Http/Customer/Controllers/OrderController.php
@@ -4,12 +4,15 @@ namespace App\Http\Customer\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Customer\Requests\CreateOrderRequest;
+use Core\Application\Contracts\Services\OrderService;
 use Core\Application\UseCases\CreateOrderUseCase;
 use Core\Application\UseCases\MakeCheckoutUseCase;
 
 class OrderController extends Controller
 {
-    public function __construct() {}
+    public function __construct(
+        private readonly OrderService $service,
+    ) {}
 
     public function create(CreateOrderRequest $request)
     {
@@ -27,5 +30,30 @@ class OrderController extends Controller
         $useCase->execute($orderUuid);
 
         return response()->json([], 204);
+    }
+
+    public function getByUuid(string $orderUuid)
+    {
+        $order = $this->service->getByUuid($orderUuid);
+
+        return response()
+            ->json(
+                [
+                    'uuid' => $order->getUuid()->getValue(),
+                    'code' => $order->getCode()->getValue(),
+                    'status' => ['uuid' => $order->getStatus()->getUuid()->getValue(), 'name' => $order->getStatus()->getName()->getValue()],
+                    'ordered_at' => $order->getOrderedAt(),
+                    'price' => $order->getPrice()->getValue(),
+                    'products' => array_map(fn($product) => [
+                        'uuid' => $product->getUuid()->getValue(),
+                        'name' => $product->getName()->getValue(),
+                        'descritption' => $product->getDescription(),
+                        'image_uri' => $product->getImageUri(),
+                        'price' => $product->getPrice()->getValue(),
+                        'category' => ['uuid' => $product->getCategory()->getUuid()->getValue(), 'name' => $product->getCategory()->getName()->getValue()],
+                    ], $order->getProducts())
+                ],
+                200,
+            );
     }
 }
