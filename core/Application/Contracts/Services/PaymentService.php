e00cb0d40e7d374fae89a582a0d250f36fc74dae
Jaymeco <jayme.anunciacao@gmail.com>
2024-12-30 21:28:44 -0300

    feature(core/Application/Contracts/Services/PaymentService): Adiciona assinatura para o método obtenção do pagamento.

--- core/Application/Contracts/Services/PaymentService.php
+++ core/Application/Contracts/Services/PaymentService.php
@@ -2,10 +2,13 @@
 
 namespace Core\Application\Contracts\Services;
 
+use Core\Application\Dtos\PaymentData;
 use Core\Application\Dtos\PaymentRequest;
 use Core\Application\Dtos\QrPaymentData;
 
 interface PaymentService
 {
     public function requestQrCode(PaymentRequest $request): QrPaymentData;
+
+    public function findPaymentById(string $id): PaymentData;
 }
