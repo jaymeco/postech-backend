2cf891967da7d3557c1b5fbc9fd3decef2a80a44
Jaymeco <jayme.anunciacao@gmail.com>
2024-12-30 21:18:40 -0300

    feature(app/Http/Mock/Controllers/MockPaymentController): Implementa controller para o mock de pagamentos.

---
+++ app/Http/Mock/Controllers/MockPaymentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Mock\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Mock\Requests\SendQrCodeRequest;
+
+class MockPaymentController extends Controller
+{
+    public function __construct() {}
+
+    public function sendQrCode(SendQrCodeRequest $request)
+    {
+        $client = new \GuzzleHttp\Client();
+        $client->post($request->getNotificationUrl(), [
+            'json' => ['data' => ['id' => 1]]
+        ]);
+
+
+        return response()->json([
+            'qr_data' => 'AA00020101021243650016COM.MERCADOLIBRE02013063638f1192a-5fd1-4180-a180-8bcae3556bc35204000053039865802BR5925IZABEL AAAA DE MELO6007BARUERI62070503***63040B6D'
+        ]);
+    }
+}
