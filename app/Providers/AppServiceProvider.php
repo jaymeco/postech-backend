149794f6ac1f6f15fd122681edd7f5b7cd0e33ee
Jaymeco <jayme.anunciacao@gmail.com>
2025-01-01 19:59:10 -0300

    refactor(app/Providers/AppServiceProvider): Registra repositorio de payment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Infra\Integrations\MockPaymentService;
 use App\Infra\Repositories\Eloquent\CategoryEloquentRepository;
 use App\Infra\Repositories\Eloquent\CustomerEloquentRepository;
 use App\Infra\Repositories\Eloquent\OrderEloquentRepository;
+use App\Infra\Repositories\Eloquent\PaymentEloquentRepository;
 use App\Infra\Repositories\Eloquent\ProductEloquentRepository;
 use App\Infra\Repositories\Memory\CustomerMemoryRepository;
 use Core\Application\Contracts\Repositories as RepositoriesContracts;
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
         RepositoriesContracts\CategoryRepository::class => CategoryEloquentRepository::class,
         RepositoriesContracts\ProductRepository::class => ProductEloquentRepository::class,
         RepositoriesContracts\OrderRepository::class => OrderEloquentRepository::class,
+        RepositoriesContracts\PaymentRepository::class => PaymentEloquentRepository::class,
 
         ServiceContracts\OrderService::class => OrderService::class,
         ServiceContracts\ProductService::class => ProductService::class,
