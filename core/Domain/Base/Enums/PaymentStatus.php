d5d692bec898db4b2c0ef148850648c758704c4b
Jaymeco <jayme.anunciacao@gmail.com>
2025-01-01 16:08:16 -0300

    feature(core/Domain/Base/Enums/PaymentStatus): Cria enumeração para os status do pagamento.

---
+++ core/Domain/Base/Enums/PaymentStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Core\Domain\Base\Enums;
+
+enum PaymentStatus: string
+{
+    case WAITING = 'AGUARDANDO_PAGAMENTO';
+    case APPROVED = 'PAGAMENTO_APROVADO';
+    case REFUSED = 'PAGAMENTO_RECUSADO';
+}
