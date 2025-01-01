c31bb292161b4a20225a7ed9bae189aaea6e4dd4
Jaymeco <jayme.anunciacao@gmail.com>
2025-01-01 16:22:12 -0300

    feature(core/Application/UseCases/ProcessPaymentUseCase): Implementa cadastro e atualização do status do pagamento do pedido.

--- core/Application/UseCases/ProcessPaymentUseCase.php
+++ core/Application/UseCases/ProcessPaymentUseCase.php
@@ -3,26 +3,59 @@
 namespace Core\Application\UseCases;
 
 use Core\Application\Contracts\Repositories\OrderRepository;
+use Core\Application\Contracts\Repositories\PaymentRepository;
 use Core\Application\Contracts\Services\PaymentService;
 use Core\Application\Dtos\ProcessPaymentDto;
+use Core\Domain\Base\Enums\PaymentStatus;
+use Core\Domain\Entities\Order;
+use Core\Domain\Entities\Payment;
+use Illuminate\Support\Facades\DB;
 
 class ProcessPaymentUseCase
 {
     public function __construct(
         private readonly OrderRepository $orderRepository,
         private readonly PaymentService $paymentService,
+        private readonly PaymentRepository $paymentRepository,
     ) {}
 
     public function execute(ProcessPaymentDto $data, string $orderUuid)
     {
-        $payment = $this->paymentService->findPaymentById($data->paymentId);
+        $paymentData = $this->paymentService->findPaymentById($data->paymentId);
 
         $order = $this->orderRepository->getByUuid($orderUuid);
+        try {
+            DB::beginTransaction();
 
-        if ($payment->isApproved()) {
-            $order->defineReceived();
+            $payment = $this->getPaymentOrCreateIfNotExists($order, $data->paymentId);
+
+            if ($paymentData->isApproved()) {
+                $order->defineReceived();
+                $payment->setPaid();
+            }
+
+            $this->paymentRepository->update($payment);
+            $this->orderRepository->update($order);
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
+
+    private function getPaymentOrCreateIfNotExists(Order $order, string $paymentId)
+    {
+        $payment = $this->paymentRepository->getActiveByOrderUuid($order->getUuid()->getValue());
+
+        if (is_null($payment)) {
+            $payment = Payment::create(
+                $order->getUuid()->getValue(),
+                $paymentId,
+                PaymentStatus::WAITING->value,
+            );
+            $this->paymentRepository->create($payment);
         }
 
-        $this->orderRepository->update($order);
+        return $payment;
     }
 }
