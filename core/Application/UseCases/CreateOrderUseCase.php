7803c7334334c70804bad3a852847d83edc075b4
Jaymeco <jayme.anunciacao@gmail.com>
2025-03-23 12:40:59 -0300

    feature(core/Application/UseCases/CreateOrdeUseCase): Adiciona autenticacao caso seja informado o cpf.

--- core/Application/UseCases/CreateOrderUseCase.php
+++ core/Application/UseCases/CreateOrderUseCase.php
@@ -2,9 +2,12 @@
 
 namespace Core\Application\UseCases;
 
+use Core\Application\Adapters\Dto\OrderDtoAdapter;
 use Core\Application\Contracts\Repositories\CustomerRepository;
 use Core\Application\Contracts\Repositories\OrderRepository;
 use Core\Application\Contracts\Repositories\ProductRepository;
+use Core\Application\Contracts\Services\AuthService;
+use Core\Application\Dtos\Order\CreateOrderOutputDto;
 use Core\Domain\Entities\Customer;
 use Core\Domain\Entities\Order;
 use Core\Domain\Entities\Product;
@@ -15,10 +18,11 @@ class CreateOrderUseCase
     public function __construct(
         private CustomerRepository $customerRepository,
         private ProductRepository $productRepository,
-        private OrderRepository $orderRepository
+        private OrderRepository $orderRepository,
+        private AuthService $authService
     ) {}
 
-    public function execute(array $products, ?string $cpf = null): Order
+    public function execute(array $products, ?string $cpf = null): CreateOrderOutputDto
     {
         DB::beginTransaction();
         try {
@@ -31,13 +35,22 @@ class CreateOrderUseCase
 
             $this->orderRepository->save($order);
 
+            $authDto = null;
+            if (!is_null($customer->getCpf())) {
+                $authDto = $this->authService->authenticate($customer->getCpf()->getValue());
+            }
+
             DB::commit();
         } catch (\Throwable $th) {
             DB::rollBack();
             throw $th;
         }
 
-        return $order;
+        return new CreateOrderOutputDto(
+            OrderDtoAdapter::parse($order),
+            !is_null($authDto) ? $authDto->code : null,
+            !is_null($authDto) ? $authDto->accessToken : null,
+        );
     }
 
     private function addProductsInOrder(array $products, Order $order)
